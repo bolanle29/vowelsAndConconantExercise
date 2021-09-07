dfd914c431156764e98dae59f9cbf6d2f843ddc2
Bolanle Ayodele <royaleunice29@gmail.com>
2021-09-07 17:26:18 +0100

    first commit

---
+++ js/obj.js
@@ -0,0 +1,30 @@
+ 
+ arr = ["a","b","c","d","e","f","g","h","i","j","k","l","m","o","p","q","r","s","t","u","v","w","x","y","z"];
+function vowelList(arr){
+   vowel=["a", "e","i","o","u"];
+   consonant=[];
+   
+   object={
+       vowel:vowel,
+       consonant:consonant
+   }
+}
+
+for (let i = 0; i < arr.length; i++) {
+    let value = arr[i] 
+} 
+// console.log(value)
+if (value.includes() ===vowel){
+    object.vowel.push(value)
+}
+
+else{
+    object.consonant.push(value)
+}
+
+console.log(vowelList(arr))
+
+
+// return object
+// const vowelList = ["a","e","i","o","u"];
+// console.log(word(vowelList))
